40e514bc185078d492ca31efe62bea2933a73fa9
pedrocelils <pedroceliob3@gmail.com>
2026-07-22 18:27:24 -0300

    commit venda

--- src/main/java/com/upoiny/kpmgv2/dto/VendaRequest.java
+++ src/main/java/com/upoiny/kpmgv2/dto/VendaRequest.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 @Getter
 @Setter
-public class VendaRequest {
-    private Long clienteId;
-    private Long funcionarioId;
-    private String formaPagamento;
-    private Double desconto;
-    private String observacoes;
-    private List<ItemVendaRequest> itens;
-}
+    public class VendaRequest {
+        private Long clienteId;
+        private Long funcionarioId;
+        private String formaPagamento;
+        private Double desconto;
+        private String observacoes;
+        private List<ItemVendaRequest> itens;
+    }
